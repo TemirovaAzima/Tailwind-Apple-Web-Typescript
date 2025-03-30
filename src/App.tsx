f8713186e6bbb6789a18e2ade71505797559f8d6
TemirovaAzima <temirovaazima9@gmail.com>
2025-03-31 02:16:33 +0500

    Home and images added in tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import Controls from "./components/Controls.tsx";
 import Navbar from "./components/Navbar.tsx";
+import Home from "./components/Home.tsx";
 
 const App: React.FC = () => {
     const [frameZoom, setFrameZoom] = useState<boolean>(false);
@@ -16,10 +17,13 @@ const App: React.FC = () => {
     return (
         <div className={"w-full h-screen grid place-items-center"}>
             <div className={` ${frameZoom ? 'min-w-[97vw] min-h-[97vh]' : 'min-w-[70vw] min-h-[85vh]'} w-[70vw] h-[85vh] max-w-[90vw] max-h-[90vh]
-                 border border-gray-200 resize overflow-auto rounded-2xl relative
+                 border border-gray-200 resize overflow-auto rounded-2xl relative flex transition-all duration-300
               `}>
                 <Controls frameZoom={frameZoom} toggleZoom={toggleZoom}/>
                 <Navbar activePage={activePage} handleNavClick={handleNavClick}/>
+                <div className='grow'>
+                    <Home/>
+                </div>
             </div>
         </div>
     )
